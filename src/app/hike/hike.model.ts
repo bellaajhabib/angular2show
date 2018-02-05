f0ce410eef005a7fcded809e78624e2b7e44401b
bellaajhabib <bellaaj.habib@gmail.com>
2018-02-05 23:31:14 +0100

    Test Pipe

---
+++ src/app/hike/hike.model.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {HikeListComponent} from './hike-list.component';
+import {HikeService} from "./hike.service";
+import {HikeDetailsComponent} from "./hike-details.component";
+import {RouterModule} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HikeFilterPipe} from "./hike-filter.pipe";
+
+
+@NgModule({
+  imports: [CommonModule,RouterModule,FormsModule],
+  declarations: [HikeListComponent,HikeDetailsComponent,HikeFilterPipe],
+  exports: [HikeListComponent],
+  providers: [HikeService]
+})
+export class HikeModule {
+}
